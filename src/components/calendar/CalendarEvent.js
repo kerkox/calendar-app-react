92b70f59fa7d1b5b776160e53a8118a7e443c103
Paul Cortes <paulkerg@gmail.com>
2021-08-19 19:41:11 -0500

    Personalizacion Eventos

---
+++ src/components/calendar/CalendarEvent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const CalendarEvent = ({ event }) => {
+  const  { title, user:{name},  } = event
+  return (
+    <div>
+      <strong>{title}</strong>
+      <span>- {name}</span>
+    </div>
+  )
+}
--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,32 +1,58 @@
-import React from 'react';
-import {Calendar, momentLocalizer} from 'react-big-calendar';
-import moment from 'moment';
-import { Navbar } from '../ui/Navbar';
-import { messages } from '../../helpers/calendar-messages-es';
+import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import { Navbar } from "../ui/Navbar";
+import { messages } from "../../helpers/calendar-messages-es";
 
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-import 'moment/locale/es';
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "moment/locale/es";
+import { CalendarEvent } from "./CalendarEvent";
 
-moment.locale('es');
+moment.locale("es");
 const localizer = momentLocalizer(moment);
-const events = [{
-  title: 'Cumpleaños Amor',
-  start: moment().toDate(),
-  end: moment().add(2, 'hours').toDate(),
-  bgcolor: "#fafafa"
-}]
+const events = [
+  {
+    title: "Cumpleaños Amor",
+    start: moment().toDate(),
+    end: moment().add(2, "hours").toDate(),
+    bgcolor: "#fafafa",
+    notes: 'Comprar pastel',
+    user: {
+      _id: '123',
+      name: 'Paul'
+    }
+  },
+];
 
 export const CalendarScreen = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    console.log(event, start, end, isSelected);
+    const style = {
+      backgroundColor: '#367CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      display: 'block',
+      color: 'white'
+    };
+    return {
+      style
+    }
+  };
   return (
     <div className="calendar-screen">
       <Navbar />
+
       <Calendar
-      localizer={localizer}
-      events={events}
-      startAccessor="start"
-      endAccessor="end"
-      messages={messages}
-    />
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        messages={messages}
+        eventPropGetter={eventStyleGetter}
+        components={{
+          event: CalendarEvent
+        }}
+      />
     </div>
-  )
-}
+  );
+};
