97a239e8d09961c114575e297834182fa8e74a4a
Paul Cortes <paul.cortesr@perficient.com>
2022-02-07 08:56:02 -0500

    Comienzo con las pruebas en React

---
+++ src/tests/types/types.test.js
@@ -0,0 +1,29 @@
+import { types } from "../../types/types";
+
+
+describe('Pruebas en Types', () => {
+  test('los types deben ser iguales ', () => {
+    expect(types).toEqual({
+      uiOpenModal: "[ui] Open modal",
+      uiCloseModal: "[ui] Close modal",
+
+      eventSetActive: "[event] Set Active",
+      eventLogout: "[event] Logout",
+
+      eventStartAddNew: "[event] Start add new",
+      eventAddNew: "[event] Add new",
+      eventClearActiveEvent: "[event] Clear Active Event",
+      eventUpdated: "[event] Event updated",
+      eventDeleted: "[event] Event deleted",
+      eventLoaded: "[event] Event loaded",
+
+      authCheckingFinish: "[auth] Finish checking login state",
+      authStartLogin: "[auth] Start login",
+      authLogin: "[auth] Login",
+      authStartRegister: "[auth] Start register",
+      authStartTokenRenew: "[auth] Start token renew",
+      authLogout: "[auth] Logout",
+    });
+  });
+  
+});
