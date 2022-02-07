0c99847fb776fc6eaff215b47f03d944f67be83d
Paul Cortes <paul.cortesr@perficient.com>
2022-02-06 23:32:55 -0500

    CRUD Eventos completo

--- src/actions/events.js
+++ src/actions/events.js
@@ -3,87 +3,99 @@ import { fetchConToken } from "../helpers/fetch";
 import { prepareEvents } from "../helpers/prepareEvents";
 import { types } from "../types/types";
 
-export const eventStartAddNew = ( event ) => {
+export const eventStartAddNew = (event) => {
   return async (dispatch, getState) => {
-
-    const {uid, name } = getState().auth;
+    const { uid, name } = getState().auth;
     try {
-
-      const resp = await fetchConToken("events", event, 'POST');
+      const resp = await fetchConToken("events", event, "POST");
       const body = await resp.json();
-      if(body.ok) {
+      if (body.ok) {
         event.id = body.evento.id;
         event.user = {
           _id: uid,
-          name
-        }
+          name,
+        };
         console.log(event);
-        dispatch(eventAddNew(event))
+        dispatch(eventAddNew(event));
       }
-    }catch(error) {
+    } catch (error) {
       console.log(error);
     }
-  }
-}
+  };
+};
 
 const eventAddNew = (event) => ({
   type: types.eventAddNew,
-  payload: event
-})
-
+  payload: event,
+});
 
 export const eventSetActive = (event) => ({
   type: types.eventSetActive,
-  payload: event
-})
+  payload: event,
+});
 
 export const eventClearActiveEvent = () => ({
-  type:types.eventClearActiveEvent
-})
+  type: types.eventClearActiveEvent,
+});
 
 export const eventStartUpdate = (event) => {
   return async (dispatch) => {
-    try{
+    try {
       const resp = await fetchConToken(`events/${event.id}`, event, "PUT");
       const body = await resp.json();
-      if(body.ok){
-        dispatch(eventUpdated(event))
+      if (body.ok) {
+        dispatch(eventUpdated(event));
       } else {
-        Swal.fire('Error', body.msg, 'error');
+        Swal.fire("Error", body.msg, "error");
       }
-    }catch(error) {
+    } catch (error) {
       console.log(error);
     }
-  }
-}
+  };
+};
 
 const eventUpdated = (event) => ({
   type: types.eventUpdated,
-  payload: event
-})
-
-export const eventDeleted = () => ({
-  type: types.eventDeleted,
+  payload: event,
 });
 
+export const eventStartDelete = () => {
+  return async (dispatch, getState) => {
+    try {
+      const { id } = getState().calendar.activeEvent
+      const resp = await fetchConToken(`events/${id}`, {}, "DELETE");
+      const body = await resp.json();
+
+      if (body.ok) {
+        dispatch(eventDeleted());
+      } else {
+        Swal.fire("Error", body.msg, "error");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+const eventDeleted = () => ({
+  type: types.eventDeleted,
+});
 
 export const eventStartLoading = () => {
   return async (dispatch) => {
     try {
-      const resp = await fetchConToken("events")
+      const resp = await fetchConToken("events");
       const body = await resp.json();
       const events = prepareEvents(body.eventos);
 
-      dispatch(eventLoaded(events))
-
-    }catch(error) {
+      dispatch(eventLoaded(events));
+    } catch (error) {
       console.log(error);
     }
-
-  }
-}
+  };
+};
 
 const eventLoaded = (events) => ({
-  type:types.eventLoaded,
-  payload: events
-})
+  type: types.eventLoaded,
+  payload: events,
+});
